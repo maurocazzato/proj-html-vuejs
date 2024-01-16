bb4052be4f2524f1c913d315c48bc5c8531b1a54
Maurocazzato <maurizzio.125@gmail.com>
2024-01-16 10:36:53 +0100

    intergrazione componenti

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <script>
+import appHeader from './components/appHeader.vue';
+import appHome from './components/appHome.vue';
+import appAbout from './components/appAbout.vue';
+import appServices from './components/appServices.vue';
+import appFooter from './components/appFooter.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    appHeader,
+    App,
+    appAbout,
+    appServices,
+    appHome,
+    appFooter,
+  },
+
+
+}
 
 </script>
 
 <template>
+    <appHeader/>
+    <appHome/>
+    <appAbout/>
+    <appServices/>
+    <appFooter/>
  
 </template>
 
