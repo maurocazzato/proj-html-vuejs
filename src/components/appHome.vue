ed42f9af71e21755d37861c27e7448cc3348f9ce
Maurocazzato <maurizzio.125@gmail.com>
2024-01-16 12:49:16 +0100

    ok home

--- src/components/appHome.vue
+++ src/components/appHome.vue
@@ -1,9 +1,16 @@
 <script>
+export default {
+  name: 'appHome',
+ 
 
+
+};
 </script>
 
 <template>
- 
+  <h1>
+    CIAOOOOOONNEEEEEEEEEEEEE sono la Home
+ </h1>
 </template>
 
 <style lang="scss" scoped>
